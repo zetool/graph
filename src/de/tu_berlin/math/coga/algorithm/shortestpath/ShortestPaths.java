d9f1f0e87392e9d16809bd6d5656ca1d2949fb90
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2014-12-01 11:59:50 +0000

    shortest paths

---
+++ src/de/tu_berlin/math/coga/algorithm/shortestpath/ShortestPaths.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.tu_berlin.math.coga.algorithm.shortestpath;
+
+import de.tu_berlin.coga.graph.Graph;
+import de.tu_berlin.coga.graph.Node;
+import de.tu_berlin.coga.container.mapping.IdentifiableIntegerMapping;
+
+/**
+ *
+ * @author gross
+ */
+public class ShortestPaths {
+
+    public ShortestPaths(Graph graph, IdentifiableIntegerMapping<Node> distances) {
+    }
+}
