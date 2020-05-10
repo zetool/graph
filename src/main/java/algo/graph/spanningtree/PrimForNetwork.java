fbc61fa354bc539fc240af565370a13730c15701
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2020-05-11 00:49:01 +0200

    Merge pull request #2 from zet-evacuation/issue-1-Convert_to_Maven_project
    Issue 1 convert to maven project

--- src/main/java/algo/graph/spanningtree/PrimForNetwork.java
+++ src/main/java/algo/graph/spanningtree/PrimForNetwork.java
@@ -1,6 +1,23 @@
+<<<<<<< HEAD:src/algo/graph/spanningtree/PrimForNetwork.java
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
+=======
+/* zet evacuation tool copyright (c) 2007-20 zet evacuation team
+ *
+ * This program is free software; you can redistribute it and/or
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+>>>>>>> 9b1f1f4... Merge pull request #2 from zet-evacuation/issue-1-Convert_to_Maven_project:src/main/java/algo/graph/spanningtree/PrimForNetwork.java
  */
 package algo.graph.spanningtree;
 
@@ -17,7 +34,7 @@ import java.util.Random;
 
 /**
  *
- * @author schwengf
+ * @author Marlen Schwengfelder
  */
 public class PrimForNetwork extends AbstractAlgorithm<NetworkMSTProblem, NetworkMST>{
     
