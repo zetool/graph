d4a56bbe446a51cc05cff4e7ea02fd0502857f97
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2020-07-24 00:21:05 +0200

    Fixed edge numbering of Prim algorithm

--- src/main/java/org/zetool/algorithm/spanningtree/PrimAlgorithm.java
+++ src/main/java/org/zetool/algorithm/spanningtree/PrimAlgorithm.java
@@ -43,6 +43,7 @@ public class PrimAlgorithm extends AbstractAlgorithm<MinSpanningTreeProblem, Und
     int NumEdge = 0;
     int overalldist = 0;
 
+    @Override
     protected UndirectedForest runAlgorithm(MinSpanningTreeProblem problem) {
 
         try {
@@ -54,19 +55,10 @@ public class PrimAlgorithm extends AbstractAlgorithm<MinSpanningTreeProblem, Und
             //gives a random start node
             Random r = new Random();
             long seed = r.nextLong();
-            seed = 5706550742198787144l; // this one creates a chain decomposition error in 3-storey 4-rooms.
-            System.out.println("Spanning Tree Seed: " + seed);
             r.setSeed(seed);
             int num = 0 + Math.abs(r.nextInt()) % numNodes;
 
-            if (num != 0) {
-                startNode = OriginNetwork.getNode(num);
-            } else {
-                startNode = OriginNetwork.getNode(num + 1);
-            }
-            System.out.println("Startknoten: " + startNode);
-            solNodes.add(startNode);
-            //distances = new IdentifiableIntegerMapping<Node>(OriginNetwork.nodeCount());
+            startNode = OriginNetwork.getNode(num);
             distances = new IdentifiableIntegerMapping<Node>(OriginNetwork.nodeCount());
             heapedges = new IdentifiableObjectMapping<Node, Edge>(OriginNetwork.edgeCount());
             MinHeap<Node, Integer> queue = new MinHeap<Node, Integer>(OriginNetwork.nodeCount());
@@ -86,15 +78,14 @@ public class PrimAlgorithm extends AbstractAlgorithm<MinSpanningTreeProblem, Und
                 solNodes.add(v);
                 distances.set(v, Integer.MIN_VALUE);
 
+                // If we are not seeing the start node, we have a cheapest edge to the node
                 if (v != startNode) {
-                    Edge edge = new Edge(NumEdge++, heapedges.get(v).start(), heapedges.get(v).end());
-                    //only consider edges that are not incident to supersink
-                    solEdges.add(edge);
+                    solEdges.add(heapedges.get(v));
                 }
                 incidentEdges = OriginNetwork.incidentEdges(v);
                 for (Edge edge : incidentEdges) {
                     Node w = edge.opposite(v);
-                    if (distances.get(w) == Integer.MAX_VALUE) {
+                    if (distances.get(w) == Integer.MAX_VALUE) { // if w is a new node
                         distances.set(w, TransitForEdge.get(edge));
                         heapedges.set(w, edge);
                         queue.insert(w, distances.get(w));
@@ -110,9 +101,7 @@ public class PrimAlgorithm extends AbstractAlgorithm<MinSpanningTreeProblem, Und
             }
 
         } catch (Exception e) {
-            System.out.println("Fehler in runMinSpan " + e.toString());
         }
-        //System.out.println("Overalldistance " + overalldist);
         return new UndirectedForest(solEdges);
 
     }
