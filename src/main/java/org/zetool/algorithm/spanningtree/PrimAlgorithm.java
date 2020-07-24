610bb4d177b35d85341c199bdcb48d60e22cca0c
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2020-07-24 23:21:46 +0200

    Refactoring and documentation of Prim's algorithm

--- src/main/java/org/zetool/algorithm/spanningtree/PrimAlgorithm.java
+++ src/main/java/org/zetool/algorithm/spanningtree/PrimAlgorithm.java
@@ -24,58 +24,73 @@ import org.zetool.container.collection.ListSequence;
 import org.zetool.container.priority.MinHeap;
 import org.zetool.graph.Node;
 
-import org.zetool.container.collection.ArraySet;
 import org.zetool.graph.Graph;
 
 /**
+ * Computes a minimum spanning tree using Prim's (or Jarník's) algorithm. If the input graph is connected, the resulting
+ * edges form a single tree. Otherwise the result is a forest containing trees.
+ * <p>
+ * Jarník, Vojtěch. <a href="https://dml.cz/bitstream/handle/10338.dmlcz/500726/Jarnik_01-0000-31_1.pdf">O jistém
+ * problému minimálním</a> (About a certain minimal problem.) Práce Moravské Přírodovědecké Společnosti, 1930, 6(4),
+ * 57–63.
+ * <p>
+ * Prim, Robert C. <a href="">Shortest connection networks And some generalizations</a>. Bell System Technical Journal,
+ * 1957, 36(6), 1389–1401.
  *
  * @author Marlen Schwengfelder
  */
 public class PrimAlgorithm extends AbstractAlgorithm<MinSpanningTreeProblem, UndirectedForest> {
 
+    /**
+     * Execute Prim's/Jarník's algorithm with a given {@link MinSpanningTreeProblem minimum spanning tree instance}.
+     *
+     * @param problem the minimum spanning tree instance
+     * @return a forest containing the edges forming one or multiple spanning trees
+     */
     @Override
     protected UndirectedForest runAlgorithm(MinSpanningTreeProblem problem) {
         Graph graph = problem.getGraph();
 
-        ArraySet<Node> vertices = new ArraySet(Node.class, problem.getGraph().nodeCount());
         IdentifiableIntegerMapping<Node> distances = new IdentifiableIntegerMapping<>(graph.nodeCount());
         IdentifiableObjectMapping<Node, Edge> heapEdges = new IdentifiableObjectMapping<>(graph.edgeCount());
 
         for (Node node : graph.nodes()) {
-            vertices.add(node);
             distances.add(node, Integer.MAX_VALUE);
             heapEdges.set(node, null);
         }
 
         IdentifiableCollection<Edge> solutionEdges = new ListSequence<>();
-        while (!vertices.isEmpty()) {
-            Node startNode = vertices.first();
-            findComponent(startNode, distances, heapEdges, solutionEdges, vertices);
+        for (Node startNode : graph.nodes()) {
+            if (distances.get(startNode) == Integer.MAX_VALUE) {
+                findComponent(startNode, distances, heapEdges, solutionEdges);
+            }
         }
 
         return new UndirectedForest(solutionEdges);
     }
 
+    /**
+     * Starting from a node detect the spanning tree containing this node.
+     *
+     * @param startNode the node to start
+     * @param distances current node distances
+     * @param heapEdges cheapest connecting edges for each node (that have been discovered before)
+     * @param solutionEdges the collection of edges in the solution
+     */
     private void findComponent(final Node startNode, final IdentifiableIntegerMapping<Node> distances,
-            final IdentifiableObjectMapping<Node, Edge> heapedges, final IdentifiableCollection<Edge> solEdges,
-            ArraySet<Node> vertices) {
+            final IdentifiableObjectMapping<Node, Edge> heapEdges, final IdentifiableCollection<Edge> solutionEdges) {
         MinHeap<Node, Integer> queue = new MinHeap<>(getProblem().getGraph().nodeCount());
-        queue.insert(startNode, 0);
 
+        handleNode(startNode, queue, distances, heapEdges);
         while (!queue.isEmpty()) {
             MinHeap<Node, Integer>.Element min = queue.extractMin();
             Node v = min.getObject();
-            vertices.remove(v);
-            // If we are not seeing the start node, we have a cheapest edge to the node
-            if (v != startNode) {
-                solEdges.add(heapedges.get(v));
-            }
-
-            handleV(v, queue, distances, heapedges);
+            solutionEdges.add(heapEdges.get(v));
+            handleNode(v, queue, distances, heapEdges);
         }
     }
 
-    private void handleV(final Node v, MinHeap<Node, Integer> queue, final IdentifiableIntegerMapping<Node> distances,
+    private void handleNode(final Node v, MinHeap<Node, Integer> queue, IdentifiableIntegerMapping<Node> distances,
             final IdentifiableObjectMapping<Node, Edge> heapEdges) {
         IdentifiableIntegerMapping<Edge> weights = getProblem().getDistances();
 
@@ -84,18 +99,42 @@ public class PrimAlgorithm extends AbstractAlgorithm<MinSpanningTreeProblem, Und
         IdentifiableCollection<Edge> incidentEdges = getProblem().getGraph().incidentEdges(v);
         for (Edge edge : incidentEdges) {
             Node w = edge.opposite(v);
-            if (distances.get(w) == Integer.MAX_VALUE) { // Node w found the first time
+            if (isConnectedFirst(w, distances)) {
+                // Node w found the first time
                 distances.set(w, weights.get(edge));
                 heapEdges.set(w, edge);
                 queue.insert(w, distances.get(w));
-            } else {
+            } else if (canBeConnectedCheaper(w, distances, edge, weights)) {
                 // Update solution edge for w, if cheaper
-                if (weights.get(edge) < distances.get(w)) {
-                    distances.set(w, weights.get(edge));
-                    queue.decreasePriority(w, weights.get(edge));
-                    heapEdges.set(w, edge);
-                }
+                distances.set(w, weights.get(edge));
+                heapEdges.set(w, edge);
+                queue.decreasePriority(w, weights.get(edge));
             }
         }
+    }
+
+    /**
+     * Checks wether a node is considered for the solution the first time.
+     *
+     * @param w the node that is considered
+     * @param distances the current node distances
+     * @return {@code true} if {@code w} is considered the first time, i.e. no connecting edge is known
+     */
+    private boolean isConnectedFirst(Node w, IdentifiableIntegerMapping<Node> distances) {
+        return distances.get(w) == Integer.MAX_VALUE;
+    }
+
+    /**
+     * Checks wether an edge can be used to connect a node cheaper as before.
+     *
+     * @param w the node that is considered
+     * @param distances the current node distances
+     * @param edge the edge that is considered
+     * @param weights the edge weights
+     * @return {@code true} if {@code w} can be cheaper connected to the tree by using {@link edge}
+     */
+    private boolean canBeConnectedCheaper(Node w, IdentifiableIntegerMapping<Node> distances, Edge edge,
+            IdentifiableIntegerMapping<Edge> weights) {
+        return weights.get(edge) < distances.get(w);
     }
 }
